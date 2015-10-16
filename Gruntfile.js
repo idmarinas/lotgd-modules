ece931748e74fe0a07206d68c4131bb44bcd497c
Iván Diaz Marinas <idmarinas@icloud.com>
2015-10-16 12:28:12 +0200

    Primera anotación

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+/*global module:false*/
+module.exports = function (grunt) {
+  "use strict";
+  // Project configuration.
+  grunt.initConfig({
+    //Información de los paquetes que se usan en el proyecto
+    project: grunt.file.readJSON('package.json'),
+    //Copiar archivos del proyecto (para publicar)
+    copy: {
+      toLocalhost: {
+        files: [
+          {
+            expand: true,
+            src: [//Archivos a copiar
+              '**',//Todos los archivos y subdirectorios
+              '!node_modules/**',//No copiar el directorio de node
+              '!bower_components/**',//No copiar el directorio de bower
+              '!*.json',//Ignorar archivos .json del directorio principal
+              '!Gruntfile.js',//Ignorar archivo Gruntfile
+              '!**/*.less',//Ignorar archivos .less
+              '!**/*.dist'//Ignorar archivos .dist
+            ],
+            dest: '/Users/Ivan/Sites/ignis/modules/',
+            // filter : 'isFile'
+          }
+        ]
+      },
+    },
+  });
+    
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task. Procesa los archivos y luego los copia en la carpeta del servidor local
+  grunt.registerTask('default', ['copy:toLocalhost']);
+};
---
+++ README.txt
@@ -0,0 +1,1 @@
+Módulos creados por otros y adaptados para funcionar en mi versiónd el juego
