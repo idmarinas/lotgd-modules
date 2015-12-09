e69cb3c64d06622c5a410054500d9c3f7b17fa3d
Iván Diaz Marinas <idmarinas@icloud.com>
2015-12-09 16:43:08 +0100

    Sólo se muestra un mensaje, y sólo se muestra si se está vivo

--- staminasystem/dohook/switch_everyhit-loggedin.php
+++ staminasystem/dohook/switch_everyhit-loggedin.php
@@ -3,31 +3,34 @@ require_once "modules/staminasystem/lib/lib.php";
 
 $amber = get_stamina();
 if ($amber < 100)
-{
+{	
 	$cansado = (!$session['user']['sex']?'cansado':'cansada');
 	$exhausto = (!$session['user']['sex']?'exhausto':'exhausta');
 	//Gives a proportionate debuff from 1 to 0.2, at 2 decimal places each time
 	$buffvalue=round(((($amber/100)*80)+20)/100,2);
-	if ($buffvalue < 1)
-	{
-		$buffmsg = "`0You're getting tired...";
-		 stamina_notification("Deberías tomarte un descanso, estás un poco $cansado.", 'La energía te abandona.', 'warning');
-	}
-	if ($buffvalue < 0.8)
-	{
-		$buffmsg = "`4You're getting `ivery`i tired...`0";
-		stamina_notification("Enserio deberías tomarte un descanso, estás muy $cansado.", 'La energía te abandona...', 'warning');
-	}
-	if ($buffvalue < 0.6)
-	{
-		$buffmsg = "`\$You're getting `bexhausted!`b`0";
-		stamina_notification("... Que no sea por que no te lo advertí, estás $exhausto.", '¿Ganas de visitar a Ramius?', 'warning');
-	}
+	$script = '';
 	if ($buffvalue < 0.3)
 	{
 		$buffmsg = "`\$You're getting `bdangerously exhausted!`b`0";
-		stamina_notification("Lo dicho, estás peligrosamente $exhausto.", 'La reunión con Ramius está cerca...', 'warning');
+		if (0 != $amber)
+			$script = stamina_notification("Lo dicho, estás peligrosamente $exhausto.", 'La reunión con Ramius está cerca...', 'warning');
 	}
+	else if ($buffvalue < 0.6)
+	{
+		$buffmsg = "`\$You're getting `bexhausted!`b`0";
+		$script = stamina_notification("... Que no sea por que no te lo advertí, estás $exhausto.", '¿Ganas de visitar a Ramius?', 'warning');
+	}
+	else if ($buffvalue < 0.8)
+	{
+		$buffmsg = "`4You're getting `ivery`i tired...`0";
+		$script = stamina_notification("Enserio deberías tomarte un descanso, estás muy $cansado.", 'La energía te abandona...', 'warning');
+	}
+	else if ($buffvalue < 1)
+	{
+		$buffmsg = "`0You're getting tired...";
+		 $script = stamina_notification("Deberías tomarte un descanso, estás un poco $cansado.", 'La energía te abandona.', 'warning');
+	}
+	
 	apply_buff('stamina-corecombat-exhaustion', array(
 		"name"=>"Exhaustion",
 		"atkmod"=>$buffvalue,
@@ -36,6 +39,9 @@ if ($amber < 100)
 		"roundmsg"=>$buffmsg,
 		"schema"=>"module-staminacorecombat"
 	));
+	
+	if ($session['user']['alive'])
+		rawoutput($script);
 } 
 else 
 {
@@ -43,16 +49,22 @@ else
 }
 
 $red = get_stamina(0);
-if ($red < 100)
+if ($session['user']['alive'])
 {
-	$death = e_rand(0,100);
-	if ($death > $red){
-		output("`\$Vision blurring, you succumb to the effects of exhaustion.  You take a step forward to strike your enemy, but instead trip over your own feet.`nAs the carpet of leaves and twigs drifts lazily up to meet your face, you close your eyes and halfheartedly reach out your hands to cushion the blow - but they sail through the ground as if it were made out of clouds.`nYou fall.`nUnconsciousness.  How you'd missed it.`0");
-		$session['user']['hitpoints']=0;
-		
-		redirect("shades.php");
+	if ($red < 100)
+	{
+		$death = e_rand(0,80);
+		if ($death > $red){
+			output("`\$Vision blurring, you succumb to the effects of exhaustion.  You take a step forward to strike your enemy, but instead trip over your own feet.`nAs the carpet of leaves and twigs drifts lazily up to meet your face, you close your eyes and halfheartedly reach out your hands to cushion the blow - but they sail through the ground as if it were made out of clouds.`nYou fall.`nUnconsciousness.  How you'd missed it.`0");
+			$session['user']['hitpoints']=0;
+			$session['user']['alive']=0;
+			
+			redirect("shades.php");
+		}
+		$script = stamina_notification('Estás en peligro, si continuas con tus acciones corres el riesgo de visitar a Ramius.', 'Ya es tarde para ti', 'danger');
+	
+		rawoutput($script);
 	}
-	stamina_notification('Estás en peligro, si continuas con tus acciones corres el riesgo de visitar a Ramius.', 'Ya es tarde para ti', 'danger');
 }
 // return true;
 
@@ -67,10 +79,10 @@ function stamina_notification($message, $title, $status)
 		UIkit.notify({
 			message : '".addslashes($message)."',
 			status  : '$status',
-			timeout : 30000,
+			timeout : 5000,
 			pos     : 'top-right'
 		});
 	</script>";	
 	
-	rawoutput($script);	
+	return $script;	
 }
