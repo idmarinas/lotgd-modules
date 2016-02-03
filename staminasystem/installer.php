568ab53bd839a3e5bb256c53d6c432cfdfbdf815
Iván Diaz Marinas <idmarinas@icloud.com>
2016-02-03 14:21:23 +0100

    Se cambia de hook

--- staminasystem/installer.php
+++ staminasystem/installer.php
@@ -4,7 +4,7 @@ if(!(is_array(unserialize(get_module_setting("actionsarray"))))){
 	set_module_setting("actionsarray",serialize(array()),"staminasystem");
 }
 
-module_addhook_priority("everyhit-loggedin",0);
+module_addhook_priority("everyfooter",0);
 module_addhook_priority("charstats",99);
 module_addhook("superuser");
 module_addhook_priority("newday",99);
