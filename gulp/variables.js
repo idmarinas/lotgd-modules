b3ddc872f46cbd70b7761dd5e0c772fe1cbe0512
Iván Diaz Marinas <idmarinas@icloud.com>
2017-01-03 17:12:48 +0100

    Se cambia la ruta del directorio local del juego

--- gulp/variables.js
+++ gulp/variables.js
@@ -2,7 +2,7 @@
  * Modify with your data
  */
 module.exports = {
-    development_dir: 'D:/MAMP/htdocs/ignis', //-- Directorio local del juego
+    development_dir: 'D:/xampp/htdocs/ignis', //-- Directorio local del juego
 	production_dir: '../production', //-- Folder when save files when you want upload to your production server
 	build_dir: 'build' //-- Name of build folder
 };
