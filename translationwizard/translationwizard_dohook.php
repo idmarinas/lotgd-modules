55468df4b1bb0b07d6976e72ea154476e53e9fa5
Iván Diaz Marinas <idmarinas@icloud.com>
2017-03-20 10:42:16 +0100

    Solucionado error, es un string

--- translationwizard/translationwizard_dohook.php
+++ translationwizard/translationwizard_dohook.php
@@ -5,10 +5,10 @@ case "superuser":
 	if ($session['user']['superuser'] & SU_IS_TRANSLATOR) {
 		addnav("Actions");
 		addnav("Translation Wizard","runmodule.php?module=translationwizard&op=list");
-		if (get_module_setting(blocktrans)) blocknav("untranslated.php");
+		if (get_module_setting('blocktrans')) blocknav("untranslated.php");
 	}
 	break;
-	
+
 case "header-modules":
 
 	if (get_module_setting("autoscan")) {
@@ -16,7 +16,7 @@ case "header-modules":
 			$languageschema=get_module_pref("language","translationwizard");
 			if (!$languageschema) break;
 			require_once("./modules/translationwizard/scanmodules_func.php");
-			$content=wizard_scanfile("modules/".httpget('module').".php"); 
+			$content=wizard_scanfile("modules/".httpget('module').".php");
 			wizard_insertfile($content,$languageschema);
 		} elseif (httpget('op')=="mass" && httppost("install")) {
 			$languageschema=get_module_pref("language","translationwizard");
@@ -47,7 +47,7 @@ case "header-modules":
 		}
 	}
 	break;
-	
+
 	/*case "footer-modules":
 		output_notl("Get:");
 		debug(httpallget());
