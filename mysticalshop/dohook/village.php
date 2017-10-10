7cdf8aa00f9769ffa896c86339feb5fe94b0ed2d
Iván Diaz Marinas <idmarinas@icloud.com>
2017-10-10 12:15:45 +0200

    Ajustes de formato

--- mysticalshop/dohook/village.php
+++ mysticalshop/dohook/village.php
@@ -6,14 +6,14 @@ addnav($args['marketnav']);
 if ($session['user']['location'] == get_module_setting("shoploc") && get_module_setting("shopall") == 0){
 	if (get_module_pref("pass") == 1 && get_module_setting("shopappear") == 1){
 		addnav("$shop",$from."op=shop&what=enter");
-	}else if (get_module_setting("shopappear") == 0 && $session['user']['dragonkills']>=get_module_setting("dkreq")){									
+	}else if (get_module_setting("shopappear") == 0 && $session['user']['dragonkills']>=get_module_setting("dkreq")){
 		addnav("$shop",$from."op=shop&what=enter");
 	}
 }else if (get_module_setting("shopall") == 1){
 	if (get_module_pref("pass") == 1 && get_module_setting("shopappear") == 1){
 			addnav("$shop",$from."op=shop&what=enter");
 	}else if (get_module_setting("shopappear") == 0
-		&& $session['user']['dragonkills']>=get_module_setting("dkreq")){									
+		&& $session['user']['dragonkills']>=get_module_setting("dkreq")){
 			addnav("$shop",$from."op=shop&what=enter");
 	}
 }
