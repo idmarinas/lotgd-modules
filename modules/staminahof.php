a46917e14a3744e1fad4bf1e93ca5323ccfe3769
Iván Diaz Marinas <idmarinas@icloud.com>
2018-02-09 12:54:16 +0100

    Se corrigen errores de visualización de las acciones

--- modules/staminahof.php
+++ modules/staminahof.php
@@ -31,24 +31,33 @@ function staminahof_dohook($hookname,$args){
 	return $args;
 }
 
-function staminahof_run(){
+function staminahof_run()
+{
 	global $session;
+
 	page_header("Action Level Rankings");
+
 	require_once "modules/staminasystem/lib/lib.php";
+
+	addnav("Exit");
+	addnav("Return to the HOF","hof.php");
 	$actions = get_default_action_list();
 
 	// Output navs to each action
-	foreach($actions AS $action => $vals){
-		addnav("Actions");
-		addnav($action,"runmodule.php?module=staminahof&action=".$action."&skip=0");
+	foreach($actions AS $action => $vals)
+	{
+		// rawurlencode
+		addnav('Actions');
+		addnav($action, "runmodule.php?module=staminahof&action=".urlencode($action)."&skip=0");
 	}
 
 
 	// Now show the HOF
 	$hof = httpget('action');
-	if ($hof){
+	if ($hof)
+	{
 		// Get action for each user
-		$hofpage = array();
+		$hofpage = [];
 		$pages = 0;
 		$numres=0;
 
@@ -63,10 +72,17 @@ function staminahof_run(){
 		for ($i=0;$i<$scount;$i++){
 			$row = DB::fetch_assoc($staminaresult);
 			$actions_array = @unserialize($row['value']);
-			$actiondetails = $actions_array[$hof];
-			if (! isset($actiondetails['exp']) || ! $actiondetails['exp']) continue;
-			$hofpage[$row['userid']]['exp'] = $actiondetails['exp'];
-			$hofpage[$row['userid']]['lvl'] = $actiondetails['lvl'];
+			if (isset($actions_array[$hof]))
+			{
+				$actiondetails = $actions_array[$hof];
+				$hofpage[$row['userid']]['exp'] = isset($actiondetails['exp']) ? $actiondetails['exp'] : 0;
+				$hofpage[$row['userid']]['lvl'] = isset($actiondetails['lvl']) ? $actiondetails['lvl'] : 0;
+			}
+			else
+			{
+				$hofpage[$row['userid']]['exp'] = 0;
+				$hofpage[$row['userid']]['lvl'] = 0;
+			}
 		}
 
 		$ncount = DB::num_rows($namesresult);
@@ -93,25 +109,21 @@ function staminahof_run(){
 		$dname = translate_inline("Name");
 		$dlvl = translate_inline("Level");
 		$drnk = translate_inline("Rank");
-		$style=0;
 		output_notl("`n`b`c`@".translate_inline($hof)."`n`n`c`b");
-		rawoutput("<table border='0' cellpadding='2' cellspacing='1' align='center' width='100%'>");
-		rawoutput("<tr class='trhead'><td align=center>$dname</td><td align=center>$drnk</td><td align=center>$dlvl</td><td align=center>$dexp</td></tr>");
+		rawoutput("<table class='ui very compact striped table'>");
+		rawoutput("<thead><tr><th>$dname</th><th>$drnk</th><th>$dlvl</th><th>$dexp</th></tr></thead>");
 
-		for($i=httpget('skip');$i<=httpget('skip')+50;$i++){
-			if ($hofpage[$i]){
-				$style++;
-				if ($hofpage[$i]['name']==$session['user']['name']){
-					rawoutput("<tr class='trhilight'><td>");
-				}else{
-					rawoutput("<tr class='".($style%2?"trdark":"trlight")."'><td align=left>");
-				}
+		for($i = httpget('skip') ; $i <= httpget('skip')+50; $i++)
+		{
+			if (isset($hofpage[$i]) && $hofpage[$i])
+			{
+				rawoutput("<tr class='".($hofpage[$i]['name']==$session['user']['name'] ? 'active' : '')."'><td>");
 				output_notl("%s",$hofpage[$i]['name']);
-				rawoutput("</td><td align=center>");
+				rawoutput("</td><td>");
 				output_notl("%s",$i+1);
-				rawoutput("</td><td align=center>");
+				rawoutput("</td><td>");
 				output_notl("%s",$hofpage[$i]['lvl']);
-				rawoutput("</td><td align=center>");
+				rawoutput("</td><td>");
 				output_notl("%s",number_format($hofpage[$i]['exp']));
 				rawoutput("</td></tr>");
 			}
@@ -120,8 +132,6 @@ function staminahof_run(){
 	} else {
 		output("`0What would you like to see?");
 	}
-	addnav("Exit");
-	addnav("Return to the HOF","hof.php");
 	page_footer();
 	return true;
 }
@@ -131,4 +141,3 @@ function staminahof_sort($x, $y){
 	else if ($x['exp'] < $y['exp']) return 1;
 	else return -1;
 }
-?>
