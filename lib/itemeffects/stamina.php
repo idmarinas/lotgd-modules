ed82d9428b18964afe1d9980eae97077c8ec0fa8
Iván Diaz Marinas <idmarinas@icloud.com>
2018-02-26 16:00:40 +0100

    Primera anotación

---
+++ lib/itemeffects/stamina.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Restore Stamina
+ *
+ * @var int $stamina Can be negative
+ *
+ * return array|false Return false if nothing happend or an array of messages
+ */
+function restore_stamina($stamina)
+{
+    //-- Not do nothing if module Stamina is NOT active
+    if (! is_module_active('staminasystem')) { return false; }
+
+    //-- No stamina restore
+    if ($stamina == 0) { return false; }
+
+    global $item;
+
+    require_once 'modules/staminasystem/lib/lib.php';
+
+    $out = [];
+
+    if ($stamina > 0)
+    {
+        $percent = get_stamina(3);
+
+        addstamina($stamina);
+
+        $staminaPercent = round(get_stamina(3) - $percent, 2);
+
+        $out[] = ['`@Restore `b%s%`b points of Stamina, about `b%s%%`b of your total Stamina by using `i%s`i.`0`n', $stamina, $staminaPercent, $item['name']];
+
+        debuglog("Restore $stamina points of Stamina by using {$item['itemid']}");
+    }
+    else
+    {
+        $percent = get_stamina(3);
+
+        removestamina(abs($stamina));
+
+        $staminaPercent = round($percent - get_stamina(3), 2);
+
+        $out[] = ['`$Lost `b%s%`b points of Stamina, about `b%s%%`b of your total Stamina by using `i%s`i.`0`n', abs($stamina), $staminaPercent, $item['name']];
+
+        debuglog("Lost $stamina points of Stamina by using {$item['itemid']}");
+    }
+
+    return $out;
+}
