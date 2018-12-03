5192a1081194c821934f759bf873ccbff0da35d2
Iván Diaz Marinas <idmarinas@icloud.com>
2018-12-03 10:51:56 +0100

    Updated config

--- .phan/config.php
+++ .phan/config.php
@@ -342,8 +342,8 @@ return [
     // Thus, both first-party and third-party code being used by
     // your application should be included in this list.
     'directory_list' => [
-        'lib/**/*',
-        'modules/**/*'
+        'lib/',
+        'modules/'
     ],
 
     // A list of individual files to include in analysis
