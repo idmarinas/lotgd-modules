71b4c7c39a0264dcd62ebd09728931a0bb8a9d32
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-01-04 22:50:33 +0100

    Formated using CS Fixer

--- modules/alignment/func.php
+++ modules/alignment/func.php
@@ -1,50 +1,43 @@
 <?php
 
-function align($val,$user=FALSE){
-	// global $session;
-	// if ($user === FALSE) $user = $session['user']['acctid'];
-	// No need for this line, as if 4th argument of increment_module_pref is false, it calls $session['user']['acctid'];
-	
-	$val = (int)$val;
-	$hook = modulehook('align-val-adjust', array('modifier'=>1));
-	if (!isset($hook['modifier']))
-		// FAIL SAFE
-		$modifier = 1;
-	else
-		$modifier = $hook['modifier'];
-	$newval = round($val * $modifier,0);
-	increment_module_pref('alignment',$newval,'alignment',$user);
+function align($val, $user = false)
+{
+    $val = (int) $val;
+    $hook = modulehook('align-val-adjust', ['modifier' => 1]);
+
+    $modifier = $hook['modifier'] ?? 1;
+    $newval = round($val * $modifier, 0);
+
+    increment_module_pref('alignment', $newval, 'alignment', $user);
 }
 
-function get_align($user=false){
-	// global $session;
-	// if ($user === FALSE) $user = $session['user']['acctid'];
-	// No need for this line, as if 4th argument of get_module_pref is false, it calls $session['user']['acctid'];
-		
-	$val = get_module_pref('alignment','alignment',$user);
-	return $val;
+function get_align($user = false)
+{
+    return get_module_pref('alignment', 'alignment', $user);
 }
 
-function set_align($val,$user=false){
-	// global $session;
-	// if ($user === FALSE) $user = $session['user']['acctid'];
-	// No need for this line, as if 4th argument of set_module_pref is false, it calls $session['user']['acctid'];
-	
-	set_module_pref('alignment',$val,'alignment',$user);
+function set_align($val, $user = false)
+{
+    set_module_pref('alignment', $val, 'alignment', $user);
 }
 
 /**
- * Get align name (code) for user alignment number
+ * Get align name (code) for user alignment number.
  */
 function get_align_name($user = false)
 {
-	$evilalign = get_module_setting('evilalign','alignment');
-	$goodalign = get_module_setting('goodalign','alignment');
-	$align = get_align($user);
-	
-	if ($goodalign <= $align) return 'good';
-	else if ($evilalign >= $align) return 'evil';
-	else return 'neutral';
-}
+    $evilalign = get_module_setting('evilalign', 'alignment');
+    $goodalign = get_module_setting('goodalign', 'alignment');
+    $align = get_align($user);
 
-?>
+    if ($goodalign <= $align)
+    {
+        return 'good';
+    }
+    elseif ($evilalign >= $align)
+    {
+        return 'evil';
+    }
+
+    return 'neutral';
+}
