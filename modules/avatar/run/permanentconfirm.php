a3d7fadcff2aefcb1c4da5e236f89f0a5eb66217
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-01-04 22:55:58 +0100

    Formated using CS Fixer

--- modules/avatar/run/permanentconfirm.php
+++ modules/avatar/run/permanentconfirm.php
@@ -1,7 +1,5 @@
 <?php
 
-	output("You've got unlimited Avatar Changes!");
-	set_module_pref("permanent",1);
-	$session['user']['donationspent']+=get_module_setting("permanent");
-
-?>
+output("You've got unlimited Avatar Changes!");
+set_module_pref('permanent', 1);
+$session['user']['donationspent'] += get_module_setting('permanent');
--- modules/avatar/wit1/setinfo.php
+++ modules/avatar/wit1/setinfo.php
@@ -1,5 +1,5 @@
 <?php
+
 $setindex = 3;
-$setname = "Set 3";
-$setcopy = "&copy; 2005 <a href='http://wits.deviantart.com'>Whitney Spencer</a>"
-?>
+$setname = 'Set 3';
+$setcopy = "&copy; 2005 <a href='http://wits.deviantart.com'>Whitney Spencer</a>";
