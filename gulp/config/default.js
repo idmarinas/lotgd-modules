fd95f42552831f867c24139245a5750076db732d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-09-18 13:35:14 +0200

    Removed unused paths

--- gulp/config/default.js
+++ gulp/config/default.js
@@ -1,15 +1,6 @@
 //-- Modify with your data
 module.exports = {
     paths: {
-        /** ***********************************************
-         * Directory for diferents test server
-         * Only need if you want copy to your localserver
-         ************************************************/
-        development: {
-            final: 'D:/xampp/htdocs/lotgd-dev', //-- Directory for final version
-            beta: 'D:/xampp/htdocs/lotgd-beta', //-- Directory for beta version
-            alpha: 'D:/xampp/htdocs/lotgd-alpha' //-- Directory for alpha version
-        },
         //-- Directory for construct game
         build: 'dist'
     },
