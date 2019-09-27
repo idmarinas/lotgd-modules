71abb58c82f816728ad798096445df83d5156193
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-09-27 13:48:18 +0200

    First commit

---
+++ src/drunkard/modules/drunkard.php
@@ -0,0 +1,100 @@
+<?php
+
+// translator ready
+// addnews ready
+// mail ready
+
+function drunkard_getmoduleinfo()
+{
+    return [
+        'name' => 'Drunkard',
+        'version' => '2.0.0',
+        'author' => 'JT Traub<br>w/ mods suggested by Jason Still, refactoring by `%IDMarinas`0, <a href="//draconia.infommo.es">draconia.infommo.es</a>',
+        'category' => 'Inn Specials',
+        'download' => 'core_module',
+        'settings' => [
+            'Drunkard Event Settings,title',
+            'spillchance' => 'Chance that the drunk spills beer on you.,range,0,100,1|30',
+            'maxseen' => 'How many times per day (0 unlimited),int|2'
+        ],
+        'prefs' => [
+            'Drunkard User Preferences,title',
+            'seen' => 'How many times has the drunkard been seen?,int|0'
+        ],
+        'require' => [
+            'lotgd' => '>=4.0.0|Need a version equal or greater than 4.0.0 IDMarinas Edition',
+        ],
+    ];
+}
+
+function drunkard_chance()
+{
+    if (get_module_pref('seen', 'drunkard') < get_module_setting('maxseen', 'drunkard'))
+    {
+        return 100;
+    }
+
+    return 0;
+}
+
+function drunkard_install()
+{
+    module_addeventhook('inn', 'require_once("modules/drunkard.php"); return drunkard_chance();');
+    module_addhook('newday');
+
+    return true;
+}
+
+function drunkard_uninstall()
+{
+    return true;
+}
+
+function drunkard_dohook($hookname, $args)
+{
+    if ('newday' == $hookname)
+    {
+        set_module_pref('seen', 0);
+    }
+
+    return $args;
+}
+
+function drunkard_runevent($type)
+{
+    global $session;
+
+    require_once 'lib/partner.php';
+
+    $chance = get_module_setting('spillchance');
+    $roll = mt_rand(1, 100);
+    $seen = get_module_pref('seen');
+    set_module_pref('seen', $seen + 1);
+
+    $textDomain = 'module-drunkard';
+
+    $params = [
+        'textDomain' => $textDomain,
+        'lucky' => false,
+        'partner' => get_partner()
+    ];
+
+    if ($roll < $chance)
+    {
+        // He spills on you
+        $session['user']['charm']--;
+        $session['user']['charm'] = max(0, $session['user']['charm']);
+    }
+    else
+    {
+        $params['lucky'] = true;
+        // You're safe
+        $session['user']['charm']++;
+    }
+
+    rawoutput(\LotgdTheme::renderModuleTemplate('drunkard/runevent.twig', $params));
+}
+
+function drunkard_run()
+{
+}
