5fdcc39d2e5a50c84ffbe348e17557e7f9bad2e8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-10-12 17:28:57 +0200

    First commit

---
+++ src/inventory/lib/itemeffects/deathpower.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Alter deathpower of character.
+ *
+ * @param int $points
+ * @param array $item
+ *
+ * @return array An array of messages
+ */
+function itemeffects_alter_deathpower($points, $item): array
+{
+    global $session;
+
+    $deathoverlord = getsetting('deathoverlord', '`$Ramius`0');
+    $session['user']['deathpower'] += $points;
+
+    if ($points > 0)
+    {
+        $out[] = [
+            'item.effect.deathpower.gain',
+            [ 'points' => $points, 'deathOverlord' => $deathoverlord, 'itemName' => $item['name'] ],
+            'module-inventory'
+        ];
+    }
+    else
+    {
+        $out[] = [
+            'item.effect.deathpower.lost',
+            [ 'points' => abs($points), 'deathOverlord' => $deathoverlord, 'itemName' => $item['name'] ],
+            'module-inventory'
+        ];
+    }
+
+    return $out;
+}
