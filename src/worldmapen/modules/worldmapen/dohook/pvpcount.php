418dcae959b06b62b48de70c64238a86b64e5639
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-11-22 17:44:39 +0100

    Deleted obsolete function

--- src/worldmapen/modules/worldmapen/dohook/pvpcount.php
+++ src/worldmapen/modules/worldmapen/dohook/pvpcount.php
@@ -4,12 +4,5 @@ if ('World' == $args['loc'])
 {
     $args['handled'] = 1;
 
-    if (1 == $args['count'])
-    {
-        output('`&There is `^1`& person camping in the wilderness whom you might find interesting.`0`n');
-    }
-    else
-    {
-        output('`&There are `^%s`& people camping in the wilderness whom you might find interesting.`0`n', $args['count']);
-    }
+    \LotgdResponse::pageAddContent(\LotgdTranslation::t('pvp.count', ['count' => $args['count']], 'module-worldmapen'));
 }
