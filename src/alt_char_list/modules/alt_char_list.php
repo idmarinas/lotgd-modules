e2fe5669a0eaa31c9acb7bd69379a15a462c8a73
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-13 13:21:35 +0100

    Updated to 4.11.0

--- src/alt_char_list/modules/alt_char_list.php
+++ src/alt_char_list/modules/alt_char_list.php
@@ -5,14 +5,14 @@ function alt_char_list_getmoduleinfo()
     return [
         'name'     => 'Alternate Character Listing',
         'author'   => 'Chris Vorndran, refactoring by `%IDMarinas`0, <a href="//draconia.infommo.es">draconia.infommo.es</a>',
-        'version'  => '2.0.0',
+        'version'  => '2.1.0',
         'category' => 'Administrative',
         'download' => 'http://dragonprime.net/index.php?module=Downloads;sa=dlview;id=1343',
         'prefs'    => [
             'allowed' => 'User allowed to view alts?,bool|0',
         ],
         'requires' => [
-            'lotgd' => '>=4.0.0|Need a version equal or greater than 4.0.0 IDMarinas Edition',
+            'lotgd' => '>=4.11.0|Need a version equal or greater than 4.11.0 IDMarinas Edition',
         ],
     ];
 }
@@ -41,8 +41,8 @@ function alt_char_list_dohook($hookname, $args)
 
             if ($allowed)
             {
-                \LotgdNavigation::addHeader('superuser.category.actions', ['textDomain' => 'navigation-app']);
-                \LotgdNavigation::addNav('navigation.nav.list', 'runmodule.php?module=alt_char_list', ['textDomain' => 'module-alt_char_list']);
+                \LotgdNavigation::addHeader('superuser.category.actions', ['textDomain' => 'navigation_app']);
+                \LotgdNavigation::addNav('navigation.nav.list', 'runmodule.php?module=alt_char_list', ['textDomain' => 'module_alt_char_list']);
             }
         break;
         case 'bioend':
@@ -50,8 +50,8 @@ function alt_char_list_dohook($hookname, $args)
 
             if ($allowed)
             {
-                \LotgdNavigation::addHeader('common.superuser.category', ['textDomain' => 'navigation-app']);
-                \LotgdNavigation::addNav('navigation.nav.view', "runmodule.php?module=alt_char_list&id={{$args['acctid']}&ret=".\urlencode($args['return_link']), ['textDomain' => 'module-alt_char_list']);
+                \LotgdNavigation::addHeader('common.superuser.category', ['textDomain' => 'navigation_app']);
+                \LotgdNavigation::addNav('navigation.nav.view', "runmodule.php?module=alt_char_list&id={{$args['acctid']}&ret=".\urlencode($args['return_link']), ['textDomain' => 'module_alt_char_list']);
             }
         break;
         default: break;
@@ -64,7 +64,7 @@ function alt_char_list_run()
 {
     global $session;
 
-    $textDomain = 'module-alt_char_list';
+    $textDomain = 'module_alt_char_list';
 
     \LotgdResponse::pageStart('title', [], $textDomain);
 
@@ -182,7 +182,7 @@ function alt_char_list_run()
 
     \LotgdNavigation::setTextDomain();
 
-    \LotgdResponse::pageAddContent(\LotgdTheme::renderModuleTemplate('alt_char_list/run.twig', $params));
+    \LotgdResponse::pageAddContent(\LotgdTheme::render('@module/alt_char_list/run.twig', $params));
 
     \LotgdResponse::pageEnd();
 }
