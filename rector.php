cf1635ab81dd201262ee8a6b74f313e662ec22af
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-02-24 12:32:53 +0100

    First commit

---
+++ rector.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
+use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
+use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\TwigSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void
+{
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+    ]);
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_73);
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
+    $parameters->set(Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER, __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+
+    $containerConfigurator->import(SetList::DEAD_CODE);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
+    $containerConfigurator->import(SetList::PHP_73);
+    $containerConfigurator->import(SetList::PHP_74);
+    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(SetList::PHP_81);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_40);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_50);
+
+    //-- Symfony Framework
+    $containerConfigurator->import(SymfonySetList::SYMFONY_40);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_41);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_42);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_43);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_44);
+    $containerConfigurator->import(TwigSetList::TWIG_240);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_25);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_COMMON_20);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_210);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_211);
+
+    //-- Skip some rules/files ...
+    $parameters->set(Option::SKIP, [
+        __DIR__.'/src/core/Twig/NodeVisitor',
+        ShortenElseIfRector::class,
+        EventListenerToEventSubscriberRector::class,
+        CallableThisArrayToAnonymousFunctionRector::class,
+        AbsolutizeRequireAndIncludePathRector::class
+    ]);
+};
