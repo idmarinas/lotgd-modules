f54030cde73eec8e638de37dec34a7d2f7a1f756
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-08-20 16:35:50 +0200

    Fixed error

--- src/statue/modules/statue.php
+++ src/statue/modules/statue.php
@@ -151,7 +151,7 @@ function statue_dohook($hookname, $args)
 
                     $args[] = [$text, $params, 'module_statue'];
 
-                    LotgdLog::addNews('news.broke.statue', [
+                    LotgdTool::addNews('news.broke.statue', [
                         'playerName' => $session['user']['name'],
                         'location'   => $session['user']['location'],
                     ], 'module_statue');
